3f16c6ed1e58ba740cbc502f40e1e5e788d667c3
Jacob Loeser <jacobharryloeser@gmail.com>
2023-12-24 10:23:07 -0500

    Buffer Swapping shortcut

--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -3,3 +3,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Fast buffer switching
+vim.keymap.set("n", "<leader>n", ":bn<CR>");
+vim.keymap.set("n", "<leader>p", ":bp<CR>");
