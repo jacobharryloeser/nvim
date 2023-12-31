2b2b5186b8274827b5b6f383133c08c3977aad13
Jacob Loeser <jacobharryloeser@gmail.com>
2023-12-30 23:41:41 -0500

    Fix Slow Keybinding due to collision.

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -19,6 +19,8 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
   textobjects = {
+
+  	  -- Beautiful Highlighting and ASTs
   	  select = {
   	  	  enable = true,
   	  	  lookahead = true,
@@ -51,12 +53,12 @@ require'nvim-treesitter.configs'.setup {
   	  swap = {
           enable = true,
           swap_next = {
-              ["<leader>na"] = "@parameter.inner", -- swap parameters/argument with next
-              ["<leader>nm"] = "@function.outer", -- swap function with next
+              ["<leader>sna"] = "@parameter.inner", -- swap parameters/argument with next
+              ["<leader>snm"] = "@function.outer", -- swap function with next
           },
           swap_previous = {
-              ["<leader>pa"] = "@parameter.inner", -- swap parameters/argument with prev
-              ["<leader>pm"] = "@function.outer", -- swap function with previous
+              ["<leader>spa"] = "@parameter.inner", -- swap parameters/argument with prev
+              ["<leader>spm"] = "@function.outer", -- swap function with previous
           },
   	  },
       move = {
