6c050d2e34f33a223746b8abfb1d8342ebaa34ed
Jacob Loeser <jacobharryloeser@gmail.com>
2024-01-14 23:41:25 -0500

    Git vagrant is pretty sweet

--- lua/main/lazy.lua
+++ lua/main/lazy.lua
@@ -1,60 +1,63 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-    -- Dope Theming
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	-- Dope Theming
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-    -- Beautiful Highlighting and ASTs
-    {
-    	'nvim-treesitter/nvim-treesitter',
-    	dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    	build = ":TSUpdate",
-    },
+	-- Beautiful Highlighting and ASTs
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+		build = ":TSUpdate",
+	},
 
-    -- Surround Text Selections
-    {
-    	'kylechui/nvim-surround',
-    	version = '*',
-    	event = 'VeryLazy',
-    },
+	-- Surround Text Selections
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+	},
 
-    -- Formatter
-    { 'mhartington/formatter.nvim' },
+	-- Super Git
+	{ "tpope/vim-fugitive" },
 
-    -- DAP (Debugging)
-    { 'mfussenegger/nvim-dap' },
-    -- JS Debugging
-    {
-    	'microsoft/vscode-js-debug',
-    	build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out'
-    },
-    { 'mxsdev/nvim-dap-vscode-js' },
+	-- Formatter
+	{ "mhartington/formatter.nvim" },
 
-    -- LSP Section
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
+	-- DAP (Debugging)
+	{ "mfussenegger/nvim-dap" },
+	-- JS Debugging
+	{
+		"microsoft/vscode-js-debug",
+		build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+	},
+	{ "mxsdev/nvim-dap-vscode-js" },
+
+	-- LSP Section
+	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "L3MON4D3/LuaSnip" },
 }, {
 	git = {
 		timeout = 300,
