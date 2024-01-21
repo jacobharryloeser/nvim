30911af0c54970674c13e059feb1ad23fd392144
Jacob Loeser <jacobharryloeser@gmail.com>
2024-01-21 13:24:02 -0500

    Explanatory comment

--- after/plugin/nvim-test.lua
+++ after/plugin/nvim-test.lua
@@ -3,4 +3,5 @@ local g = vim.g
 g["test#strategy"] = "neovim_sticky"
 g["test#neovim#term_position"] = "vertical rightb"
 
+-- Better Mapping to kill terminal mode
 vim.keymap.set("t", "<C-o>", "<C-\\><C-n>")
