987813ed547326258ab43b91a349fdb047228d72
Jacob Loeser <jacobharryloeser@gmail.com>
2024-02-05 19:26:09 -0500

    Experimental live buffer tail for long running processes

--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -35,4 +35,31 @@ local function scratch()
 	end)
 end
 
-vim.keymap.set("n", "<leader>sc", scratch, { desc = "Command to scratch buffer sync mode" })
+vim.keymap.set("n", "<leader>sc", scratch, { desc = "Command to scratch buffer async mode" })
+
+local function live_scratch()
+	local buf = vim.api.nvim_create_buf(true, true)
+
+	local function write_to_scratch(_, data, _)
+		local output = {}
+		for _, v in pairs(data) do
+			table.insert(output, v)
+		end
+		vim.api.nvim_buf_set_lines(buf, -1, -1, false, output)
+		local win = vim.api.nvim_get_current_win()
+		vim.api.nvim_win_set_buf(win, buf)
+	end
+
+	vim.ui.input({ prompt = "enter command> ", completion = "command" }, function(input)
+		if input == nil then
+			return
+		elseif input == "scratch" then
+			input = "echo('')"
+		end
+		print("Running your command, so do some push ups and some squats.")
+		vim.fn.jobstart(input, { on_stdout = write_to_scratch, on_stderr = write_to_scratch })
+		vim.cmd("vsplit")
+	end)
+end
+
+vim.keymap.set("n", "<leader>sd", live_scratch, { desc = "Command to scratch buffer live async mode" })
