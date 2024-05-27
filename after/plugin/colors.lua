b147efe46e43a9dd03251874dcc4a5e2f1674eca
Jacob Loeser <jacobharryloeser@gmail.com>
2024-05-26 20:30:24 -0400

    Updates broke transparency plugin :(
    Too lazy to research right now, so plugging back in synthwave

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,10 @@
--- function ColorMyPencils()
--- 	vim.o.termguicolors = true
--- 	local config = require("synthwave84.theme").load()
--- 	local highlights = require("synthwave84.highlights")
---
--- 	highlights.set_highlights(config.glow)
--- 	vim.cmd.colorscheme("synthwave84")
--- end
---
--- ColorMyPencils()
+function ColorMyPencils()
+	vim.o.termguicolors = true
+	local config = require("synthwave84.theme").load()
+	local highlights = require("synthwave84.highlights")
+
+	highlights.set_highlights(config.glow)
+	vim.cmd.colorscheme("synthwave84")
+end
+
+ColorMyPencils()
