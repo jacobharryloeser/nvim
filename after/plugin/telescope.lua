a95e894afd10d4cfa01f82210c46ca2377d174b5
Jacob Loeser <jacobharryloeser@gmail.com>
2024-07-14 15:47:53 -0400

    Add hidden files to ripgrep searches

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,19 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find all of the files" })
+
+vim.keymap.set("n", "<leader>ff", function()
+	builtin.find_files({ hidden = true })
+end, { desc = "Find all of the files" })
+
 vim.keymap.set("n", "<C-p>ff", builtin.git_files, { desc = "Find all source controlled files. Not used often." })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Ripgrep through file content" })
+
+vim.keymap.set("n", "<leader>fg", function()
+	builtin.live_grep({ hidden = true })
+end, { desc = "Ripgrep through file content" })
+
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Lets search the Help Docs" })
+
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Look at hella buffers" })
+
 vim.keymap.set("n", "<leader>fs", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, { desc = "Ripgrep through file content without live updates" })
