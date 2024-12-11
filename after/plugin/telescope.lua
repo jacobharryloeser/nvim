67dc9fac030e0d8c984ce16422f6141e6736e8a1
Jacob Loeser <jacobharryloeser@gmail.com>
2024-12-10 21:30:13 -0500

    Removed hidden file searching since it's noisy. Should add back for certain repos.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,13 +1,13 @@
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>ff", function()
-	builtin.find_files({ hidden = true })
+	builtin.find_files({})
 end, { desc = "Find all of the files" })
 
 vim.keymap.set("n", "<C-p>ff", builtin.git_files, { desc = "Find all source controlled files. Not used often." })
 
 vim.keymap.set("n", "<leader>fg", function()
-	builtin.live_grep({ hidden = true })
+	builtin.live_grep({})
 end, { desc = "Ripgrep through file content" })
 
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Lets search the Help Docs" })
