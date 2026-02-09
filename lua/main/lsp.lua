d18deab419f85c652884197bf815414b30b80bd8
Jacob Loeser <jacobharryloeser@gmail.com>
2026-02-09 10:05:54 -0500

    Native LSP Support is a lil easier

--- lua/main/lsp.lua
+++ lua/main/lsp.lua
@@ -22,3 +22,21 @@ vim.lsp.config["lua_ls"] = {
 }
 
 vim.lsp.enable("lua_ls")
+
+-- Unfortunately installed globally via npm instead of as a local binary
+-- npm i -g typescript-language-server typescript
+vim.lsp.config["tsserver"] = {
+	cmd = { "typescript-language-server", "--stdio" },
+	filetypes = { "javascript", "typescript" },
+	root_marker = { ".git", "package.json", "tsconfig.json" },
+}
+
+vim.lsp.enable("tsserver")
+
+vim.lsp.config["clangd"] = {
+	cmd = { "clangd" },
+	filetypes = { "c", "cpp" },
+	root_marker = { ".git", ".clangd", "compile_commands.json" },
+}
+
+vim.lsp.enable("clangd")
